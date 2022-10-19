b6cf9b3a2f88c9538c569a84dd914804a1fd2b6f
jinweihua@axzo.cn <JIN210714>
2022-10-19 11:43:37 +0800

    v1.0.1

--- app/src/main/java/com/king/mygitflowtest/MainActivity.kt
+++ app/src/main/java/com/king/mygitflowtest/MainActivity.kt
@@ -11,6 +11,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         Log.e(TAG, "onCreate:  release v1.0.0")
+        Log.e(TAG, "onCreate:  release v1.0.1")
 
     }
 }
